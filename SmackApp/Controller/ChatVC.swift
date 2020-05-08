303ca2926e9abf29ffcbd42c5d615ca4a4e509e3
Gustavo <gustavo.goncalvespires@gmail.com>
2020-05-08 00:28:26 -0300

    Moved files to App's directory and changed into the Build Settings (thanks StackOverflow) + Added SWReveal funcionality for menu items

---
+++ SmackApp/Controller/ChatVC.swift
@@ -0,0 +1,34 @@
+//
+//  ChatVC.swift
+//  SmackApp
+//
+//  Created by Gustavo Mac Mini on 08/05/20.
+//  Copyright © 2020 DEVX. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
