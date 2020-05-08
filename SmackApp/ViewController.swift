ebf9dd65e5c66f5e044e8a9291c26268a4b3802c
Gustavo <gustavo.goncalvespires@gmail.com>
2020-05-07 23:49:36 -0300

    Initial commit and setup

---
+++ SmackApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmackApp
+//
+//  Created by Gustavo Mac Mini on 07/05/20.
+//  Copyright © 2020 DEVX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
